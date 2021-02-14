beccc8cd76f7f6adcd7167c3a4f6e42d9c33f4b1
Alfred Sta Iglesia <a.sta.iglesia@gmail.com>
2021-02-14 13:59:15 -0800

    create DeckView file structure, insert AddCard comments

--- client/src/pages/AddCard/AddCard.jsx
+++ client/src/pages/AddCard/AddCard.jsx
@@ -11,28 +11,37 @@ class AddCard extends Component {
         definition: '',
         deck: ''
     }
-  this.handleSubmit = this.handleSubmit.bind(this);
-  this.handleTerm = this.handleTerm.bind(this);
-  this.handleDefinition = this.handleDefinition.bind(this);
-  this.handleDeck = this.handleDeck.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleTerm = this.handleTerm.bind(this);
+    this.handleDefinition = this.handleDefinition.bind(this);
+    this.handleDeck = this.handleDeck.bind(this);
   }
+
   // helper functions to handle entries
+  // function to setState of term on field entry
   handleTerm(event) {
     event.preventDefault();
     this.setState({term: event.target.value})
-    // console.log(this.state.cardForm)
   }
 
+  // function to setState of definition on field entry
   handleDefinition(event) {
     event.preventDefault();
     this.setState({definition: event.target.value})
   }
 
+  // function to setState of deck on field entry
   handleDeck(event) {
     event.preventDefault();
     this.setState({deck: event.target.value})
   }
-
+  
+  // provide onclick POST functionality to submit button
+  // - function to instantiate an object consisting of data from state object
+  // --- term
+  // --- definition 
+  // --- deck
+  // - POSt object to route /card
   handleSubmit() {
     let term = this.state.term
     let definition = this.state.definition
@@ -54,7 +63,7 @@ class AddCard extends Component {
     })
   }
 
-
+  // render add card component consisiting of a heading, input fields and submit button
   render () {
     let context = this;
     return (
@@ -62,23 +71,25 @@ class AddCard extends Component {
         {/* // card form h2 "CARD FORM" */}
         <h2 className="addcard">Create A New Flash Card</h2>
 
-
+        {/* create a card entry field with three inputs */}
         <form className="cardform">
             <div  className="mb-3">
-            <label htmlFor="formGroupExampleInput"  className="form-label">Term</label>
-            <input type="text"  className="form-control" id="formGroupExampleInput" placeholder="YetiCrab" onChange={(e) => context.handleTerm(event)}/>
+              <label htmlFor="formGroupExampleInput"  className="form-label">Term</label>
+              <input type="text"  className="form-control" id="formGroupExampleInput" placeholder="YetiCrab" onChange={(e) => context.handleTerm(event)}/>
             </div>
             
             <div  className="mb-3">
-            <label htmlFor="formGroupExampleInput2"  className="form-label">Definition</label>
-            <input type="text"  className="form-control" id="formGroupExampleInput2" placeholder="Kiwa hirsuta is a crustacean discovered in 2005 in the South Pacific Ocean. This decapod, which is approximately 15 cm long, is notable for the quantity of silky blond setae covering its pereiopods. Its discoverers dubbed it the yeti lobster or yeti crab" onChange={(e) => context.handleDefinition(event)}/>
+              <label htmlFor="formGroupExampleInput2"  className="form-label">Definition</label>
+              <input type="text"  className="form-control" id="formGroupExampleInput2" placeholder="Kiwa hirsuta is a crustacean discovered in 2005 in the South Pacific Ocean. This decapod, which is approximately 15 cm long, is notable for the quantity of silky blond setae covering its pereiopods. Its discoverers dubbed it the yeti lobster or yeti crab" onChange={(e) => context.handleDefinition(event)}/>
             </div>
             
             <div  className="mb-3">
-            <label htmlFor="formGroupExampleInput2"  className="form-label">Deck</label>
-            <input type="text"  className="form-control" id="formGroupExampleInput2" placeholder="Crustaceans" onChange={(e) => context.handleDeck(event)}/>
+              <label htmlFor="formGroupExampleInput2"  className="form-label">Deck</label>
+              <input type="text"  className="form-control" id="formGroupExampleInput2" placeholder="Crustaceans" onChange={(e) => context.handleDeck(event)}/>
             </div>
         </form>
+
+        {/* submit button to provide onclick functionality sending data entry object to backend for storage */}
         <button  className="submitButton" onClick={context.handleSubmit}>+</button>
         </div>
             )  
@@ -88,30 +99,3 @@ class AddCard extends Component {
 export default AddCard;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*{ <label htmlFor="term">Term :</label> <br></br>
-<span><input type="text" className="termvalue"/> </span>    
-<p></p>
-<label htmlFor="definition">Definition :</label> <br></br>
-<span><input type="text" className="termvalue"/> </span> 
-<p></p>
-<label htmlFor="deck">Deck Label :</label> <br></br>
-<span><input type="text" className="termvalue"/> </span>  }*/
