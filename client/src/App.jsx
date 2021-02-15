95937f70b1d095bb693d81b848ca3a168c9e59e3
Alfred Sta Iglesia <a.sta.iglesia@gmail.com>
2021-02-14 18:18:52 -0800

    progress draft

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,45 @@
 /* eslint-disable prettier/prettier */
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import { Router, Route, Switch } from 'react-router';
 import Header from "./components/Header";
 import AddCard from "./pages/AddCard";
+import DeckView from "./pages/DeckView";
+
+
+// ### implement React-Router to render between AddCard and DeckView
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <Header/>
+
+//         <Router>
+//           <Route exact path="/" component={AddCard}/>
+//           <Route path="/deckview" component={DeckView}/>
+//         </Router>
+        
+
+//       </div>
+//     )
+//   }
+// }
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header/>
+
         <AddCard/>
+        <DeckView/>
+
+        
+
       </div>
     )
   }
-}
+} 
 
 export default App;
